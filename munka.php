758e18a73cdb2612cd2c37936a1bd1b05d8d2d9a
viraggy <viraggy58@gmail.com>
2018-07-02 19:55:13 +0200

    Változott a kód itt is...

--- munka.php
+++ munka.php
@@ -6,8 +6,8 @@
 //Összeadás
 //-------------------
 
-$a = 10;
-$b = 15;
+$a = 105;
+$b = 150;
 
 print $a + $b;
 
@@ -21,6 +21,6 @@ $y = 19;
 print "<br /><br />" . $x - $y;
 
 
-
+print "Konfliktus lessz......";
 
 ?>
